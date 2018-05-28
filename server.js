43fff6f24f92f90f91667dee7c99b34c469b37b4
Kabir Maan <kabirmaan11@gmail.com>
2018-05-28 20:02:59 +0100

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -34,5 +35,14 @@ app.use(bodyParser.json());
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
